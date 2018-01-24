797432d7acca844d2ddbc2e92ab131f7dd13861e
fowo89 <34455063+fowo89@users.noreply.github.com>
2018-01-24 20:04:26 +0100

    Add files via upload

--- Ugani_skrito_stevilko.py
+++ Ugani_skrito_stevilko.py
@@ -2,15 +2,22 @@
 
 import random
 
-secret = random.randint(1,100)
+def main():
 
-guess = int(raw_input("Ugani skrito številko: "))
+    secret = random.randint(1,100)
+    print (secret)
 
-if secret == guess:
-    print"Res je, skrita številka je %s! Čestitamo!:)" % secret
-else:
-    print "Vaše ugibanje je bilo žal neuspešno. Poizkusite znova"
+    guess = int(raw_input("Ugibaj skrito številko: "))
+    st_ugibanj = 0
 
+    while st_ugibanj < 5:
+        if guess != secret:
+            print "Vaše ugibanje je bilo žal neuspešno. Skrita številka ni " + str(guess)
+            guess = int(raw_input("Ugibaj še enkrat: "))
+            st_ugibanj = st_ugibanj + 1
+        elif guess == secret:
+            print "Bravo, uganili ste!"
+            break
+if __name__ == '__main__':
+    main()
 
-#prava številka
-print secret
